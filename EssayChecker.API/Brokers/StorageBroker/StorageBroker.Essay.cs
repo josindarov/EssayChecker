8c5dea0f8a2bdf3003ad52dfdabe1882400d4bd8
Jurabek <jorabeksindarov44@gmail.com>
2023-11-25 13:10:47 +0500

    BROKER: IStorageBroker interface implemented

--- EssayChecker.API/Brokers/StorageBroker/StorageBroker.Essay.cs
+++ EssayChecker.API/Brokers/StorageBroker/StorageBroker.Essay.cs
@@ -1,6 +1,26 @@
+using EssayChecker.API.Models.Essays;
+
 namespace EssayChecker.API.Brokers.StorageBroker;
 
-public partial class StorageBroker
+public partial class StorageBroker : IStorageBroker
 {
-    
+    public async ValueTask<Essay> InsertEssayAsync(Essay essay)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<Essay> SelectAllEssays()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<Essay> SelectEssayByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<Essay> DeleteEssayAsync(Essay essay)
+    {
+        throw new NotImplementedException();
+    }
 }
--- EssayChecker.API/Brokers/StorageBroker/StorageBroker.Feedback.cs
+++ EssayChecker.API/Brokers/StorageBroker/StorageBroker.Feedback.cs
@@ -1,6 +1,21 @@
+using EssayChecker.API.Models.Feedbacks;
+
 namespace EssayChecker.API.Brokers.StorageBroker;
 
-public partial class StorageBroker
+public partial class StorageBroker : IStorageBroker
 {
-    
+    public async ValueTask<Feedback> InsertFeedbackAsync(Feedback feedback)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<Feedback> SelectAllFeedbacks()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<Feedback> SelectFeedbackById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
 }
