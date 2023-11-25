4c180c4b4381e381bc30dc355a02773b12620b9b
Jurabek <jorabeksindarov44@gmail.com>
2023-11-25 13:17:22 +0500

    CODE RUB: StorageBroker.Essay methods implemented

--- EssayChecker.API/Brokers/StorageBroker/StorageBroker.Essay.cs
+++ EssayChecker.API/Brokers/StorageBroker/StorageBroker.Essay.cs
@@ -1,26 +1,21 @@
 using EssayChecker.API.Models.Essays;
+using Microsoft.EntityFrameworkCore;
 
 namespace EssayChecker.API.Brokers.StorageBroker;
 
 public partial class StorageBroker : IStorageBroker
 {
-    public async ValueTask<Essay> InsertEssayAsync(Essay essay)
-    {
-        throw new NotImplementedException();
-    }
+    public DbSet<Essay> Essays { get; set; }
 
-    public IQueryable<Essay> SelectAllEssays()
-    {
-        throw new NotImplementedException();
-    }
+    public async ValueTask<Essay> InsertEssayAsync(Essay essay) =>
+        await InsertAsync(essay);
 
-    public async ValueTask<Essay> SelectEssayByIdAsync(Guid id)
-    {
-        throw new NotImplementedException();
-    }
+    public IQueryable<Essay> SelectAllEssays() =>
+         SelectAll<Essay>();
 
-    public async ValueTask<Essay> DeleteEssayAsync(Essay essay)
-    {
-        throw new NotImplementedException();
-    }
+    public async ValueTask<Essay> SelectEssayByIdAsync(Guid id) =>
+        await SelectAsync<Essay>(id);
+
+    public async ValueTask<Essay> DeleteEssayAsync(Essay essay) =>
+        await DeleteAsync<Essay>(essay);
 }
